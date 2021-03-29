7164eb2a6c69fd32612264bdf725b37331389a92
zhaned <zhanedmond@gmail.com>
2021-03-29 01:42:51 -0700

    inital commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,47 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn(),
+  },
+}));
+
+describe('03_separation-of-concerns-demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new order in our database and sends a text message', () => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 })
+      .then((res) => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 10,
+        });
+      });
+  });
+
+  it('ASYNC/AWAIT: creates a new order in our database and sends a text message', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
+    expect(res.body).toEqual({
+      id: '1',
+      quantity: 10,
+    });
+  });
+
+  it('get by id', async() => {
+    const order = await Order.insert({ quantity: 10 });
+
+    const res = await request(app)
+      .get(`/api/v1/orders/${order.id}`);
+  })
+});
