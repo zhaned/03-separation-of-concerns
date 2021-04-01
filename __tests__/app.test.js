688f9e03d7c3a3cfc59951daf5a5a9041993359a
zhaned <zhanedmond@gmail.com>
2021-04-01 00:07:15 -0700

    finished up lab 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order.js');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -38,10 +39,48 @@ describe('03_separation-of-concerns-demo routes', () => {
     });
   });
 
-  it('get by id', async() => {
-    const order = await Order.insert({ quantity: 10 });
-
+  it('ASYNC/AWAIT: gets all the orders', async () => {
+    const order1 = await Order.insert({ quantity: 6 });
+    const order2 = await Order.insert({ quantity: 4 });
     const res = await request(app)
-      .get(`/api/v1/orders/${order.id}`);
-  })
+      .get('/api/v1/orders')
+    
+    expect(res.body).toEqual([order1, order2]);
+  });
+
+  // it('get by id', async() => {
+  //   const order = await Order.insert({ quantity: 10 });
+
+  //   const res = await request(app)
+  //     .get(`/api/v1/orders/${order.id}`);
+  // })
+  it('ASYNC/AWAIT: gets an order via id', async () => {
+    const order1 = await Order.insert({ quantity: 6 });
+    const res = await request(app)
+      .get('/api/v1/orders/1');
+
+    expect(res.body).toEqual(order1);
+  });
+
+  it('ASYNC/AWAIT: updates an order with new quantity via id', async () => {
+    await Order.insert({ quantity: 4 });
+    const res = await request(app)
+      .put('/api/v1/orders/1')
+      .send({ quantity: 14 })
+      
+    const updatedOrder = await request(app)
+    .get('/api/v1/orders/1')
+
+    expect(res.body).toEqual(updatedOrder.body);
+  });
+
+  it('ASYNC/AWAIT: deletes an order via id', async () => {
+    const order1 = await Order.insert({ quantity: 6 });
+    const res = await request(app)
+      .delete('/api/v1/orders/1')
+      
+
+    expect(res.body).toEqual(order1);
+  });
+
 });
